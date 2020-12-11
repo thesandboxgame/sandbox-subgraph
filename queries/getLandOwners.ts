932f7c0a5c62e173510bc9c450d7f9a520d53037
Wighawag <wighawag@gmail.com>
2020-12-11 16:01:59 +0000

    add query to get land owner

---
+++ queries/getLandOwners.ts
@@ -0,0 +1,54 @@
+import 'dotenv/config';
+import 'isomorphic-unfetch';
+import {createClient} from '@urql/core';
+
+async function query<T>(queryString: string, field: string, variables: Record<string, unknown>): Promise<T[]> {
+  const first = 100;
+  let lastId = "0x0";
+  let numEntries = first;
+  let entries: T[]= [];
+  while(numEntries === first) {
+    const result = await client.query(queryString, {first, lastId, ...variables}).toPromise();
+    const data = result.data;
+    let newEntries = [];
+    if (data) {
+      newEntries = data[field];
+    }
+    if (!entries) {
+      newEntries = [];
+    }
+    numEntries = newEntries.length;
+    if (numEntries > 0) {
+      const newLastId = newEntries[numEntries - 1].id;
+      if (lastId === newLastId) {
+        console.log("same query, stop");
+        break;
+      }
+      lastId = newLastId;
+    }
+    entries = entries.concat(newEntries);
+  }
+  return entries
+}
+
+const client = createClient({
+  url: 'https://api.thegraph.com/subgraphs/name/pixowl/the-sandbox',
+});
+
+
+const queryString = `
+query($blockNumber: Int! $first: Int! $lastId: ID!) {
+    owners(first: $first where: {numLands_gt: 0 id_gt: $lastId} block: {number: $blockNumber}) {
+      id
+      numLands
+    }
+}
+`;
+
+const blockNumber = 11432307;
+
+(async() => {
+    const landOwners = await query(queryString, "owners", {blockNumber});
+    console.log(landOwners.length);
+})()
+
